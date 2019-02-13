fccb64ec69ec57f2e1d0ea2f8bacf1a10a6554f1
Hendrik Tekayadi <hendrik@insignia.vc>
2019-02-13 17:29:00 +0800

    Profile page styling

--- frontend/components/profile/profile_page.jsx
+++ frontend/components/profile/profile_page.jsx
@@ -10,37 +10,45 @@ class ProfilePage extends React.Component {
     super(props)
   }
 
+  componentWillMount() {
+    this.props.requestTopics();
+  }
+
   render () {
     const { user, topics } = this.props;
+    console.log("topics 123");
+    console.log(topics);
 
     const topicItems = topics.map( topic => (
-      <li key={ "topic-" + topic.id }>
+      <li className="profile-question-list-item" key={ "topic-" + topic.id }>
         <Link to={`/topics/${topic.id}`}>{topic.name}</Link>
         <FollowTopicButtonContainer id={topic.id} followerIds={topic.follower_ids} followed={topic.followed}/>
       </li>
     ));
 
     return (
-      <div>
-        <div className="greeting">
+      <div id="topics-container">
+        <ul className="topic-list">
+          <div className="greeting">
 
-          <div className="greeting-header">
-            <img src={user.pro_pic_url} alt={`${user.name}'s picture`}  className="user-pro-pic" />
-            <span>{user.name}</span>
+            <div className="greeting-header">
+              <img src={user.pro_pic_url} alt={`${user.name}'s picture`}  className="user-pro-pic" />
+              <span>{user.name}</span>
+            </div>
+
           </div>
 
-        </div>
-
-        <div className="feed-sidebar-header">
-          <div className= "feed-sidebar-fixed">
+           <li className="topic-list-item">
             <h2>Following Topics</h2>
-          </div>
-        </div>
+            <ul className="profile-question-list">{topicItems}</ul>
 
-        <ul className="sidebar-topic-list">
-          {topicItems}
+          </li>
+
+
         </ul>
       </div>
+
+
     );
 
   }
