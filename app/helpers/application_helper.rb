bdce8e23a443fe67fcabdd7fd7459e7159f4e6e5
Aldi Doanta <aldi@insignia.vc>
2019-02-21 23:18:38 +0700

    Application helper scaffolding for converting image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def self.get_base64_image(string)
+
+  end
+
+  def self.convert_base64_image(image_url)
+    if (!image_url)
+      return image_url
+    end
+    uri = URI(image_url)
+    serverHost = ENV.fetch('AWS_BUCKET') + '.s3-' + ENV.fetch('AWS_REGION') + '.amazonaws.com'
+    dir = 'images/'
+    file_key = Digest::MD5.hexdigest(image_url)
+    file_object = open(image_url)
+    conversion_result = self.upload_to_rafael_s3(dir + file_key, file_object, 'public-read')
+    if (conversion_result['is_success'])
+      image_url = conversion_result['aws_url']
+    end
+    return image_url
+  end
+
 end
