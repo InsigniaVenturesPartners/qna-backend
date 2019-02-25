5c5a4887c5dbd6383367edef1d383bf9e1a9c71b
Hendrik Tekayadi <hendrik@insignia.vc>
2019-02-25 14:11:02 +0700

    Clean unused methods

--- frontend/components/nav_bar/admin_nav_bar.jsx
+++ frontend/components/nav_bar/admin_nav_bar.jsx
@@ -11,72 +11,12 @@ import QuestionSearchContainer from '../question_search/question_search_containe
 class AdminNavBar extends React.Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      createModalIsOpen: false,
-      successModalIsOpen: false,
-      question: "",
-      asked_question: {},
-      checkedTopics: new Map()
-    };
-
-    this.setQuestion = this.setQuestion.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleSuccessfulSubmit = this.handleSuccessfulSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-
-    this.openModal = this.openModal.bind(this);
-    // this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
   }
 
   componentWillMount() {
     if (this.props.user.role !== "admin") {
       this.props.history.push('/');
     }
-  }
-
-  openModal(modalName) {
-    let desiredState = {question: ""};
-    desiredState[modalName+"ModalIsOpen"] = true;
-    this.setState(desiredState);
-  }
-
-
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    // this.subtitle.style.color = '#f00';
-  }
-
-  closeModal(modalName) {
-    let desiredState = {};
-    desiredState[modalName+"ModalIsOpen"] = false;
-    this.setState(desiredState);
-  }
-
-  setQuestion(e) {
-    let question = e.target.value ? e.target.value : "";
-    question = question.charAt(0).toUpperCase() + question.slice(1);
-    this.setState({ question });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.createQuestion(this.state.question, Array.from(this.state.checkedTopics.keys())).then(
-      question => this.handleSuccessfulSubmit(question.question)
-    );
-  }
-
-  handleSuccessfulSubmit(question) {
-    this.closeModal("create");
-    this.setState({asked_question: question, question: "", checkedTopics: new Map()})
-    this.openModal("success")
-  }
-
-  handleChange(e) {
-    const item = e.target.name;
-    const isChecked = e.target.checked;
-    this.setState(prevState => ({ checkedItems: prevState.checkedTopics.set(item, isChecked) }));
   }
 
   render() {
