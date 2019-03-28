a4b677b2b3d5303728c4563c8285bca184cd809b
Hendrik Tekayadi <hendrik@insignia.vc>
2019-03-28 11:01:26 +0800

    Add sign in count to users

--- db/migrate/20190328021952_add_provider_to_users.rb
+++ db/migrate/20190328021952_add_provider_to_users.rb
@@ -7,11 +7,5 @@ class AddProviderToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
     add_column :users, :confirmation_token, :string
-
-    add_column :users, :sign_in_count, :integer
-    add_column :users, :current_sign_in_at, :datetime
-    add_column :users, :last_sign_in_at, :datetime
-    add_column :users, :current_sign_in_ip, :inet
-    add_column :users, :last_sign_in_ip, :inet
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190328021952) do
+ActiveRecord::Schema.define(version: 20190328021982) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -126,6 +126,11 @@ ActiveRecord::Schema.define(version: 20190328021952) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "confirmation_token"
+    t.integer "sign_in_count"
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["google_id"], name: "index_users_on_google_id", unique: true
   end
