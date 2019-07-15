8377b973e3a199d1bb87dec48d19393410f24f31
Indra <indra@insignia.vc>
2019-07-15 21:01:15 +0700

    give date filter

--- app/controllers/api/v1/reporting_controller.rb
+++ app/controllers/api/v1/reporting_controller.rb
@@ -17,23 +17,43 @@ class Api::V1::ReportingController < Api::V1::BaseController
   end
 
   def total_question_posted
-    question = Question.group('DATE(created_at)').order('DATE(created_at) DESC').count
+    question = Question.group('DATE(created_at)').order('DATE(created_at) DESC')
+    if params[:ds]
+      question = question.where('DATE(created_at) >= ?', params[:ds])
+    end
+    if params[:de]
+      question = question.where('DATE(created_at) <= ?', params[:de])
+    end
+    question = question.count
     return render :json => question
   end
 
   def total_answer_posted
-    answer = Answer.where(question_id: Question.select('id')).group('DATE(created_at)').order('DATE(created_at) DESC').count
+    answer = Answer.where(question_id: Question.select('id')).group('DATE(created_at)').order('DATE(created_at) DESC')
+    if params[:ds]
+      answer = answer.where('DATE(created_at) >= ?', params[:ds])
+    end
+    if params[:de]
+      answer = answer.where('DATE(created_at) <= ?', params[:de])
+    end
+    answer = answer.count
     return render :json => answer
   end
 
   def insignia_answer_posted
     answer = Question.select('questions.id, questions.body,
       answers.id as answer_id, answers.body as answer_body,
-      answers.created_at, answers.updated_at, 
+      answers.created_at as answer_created_at, answers.updated_at as answer_updated_at, 
       users.id as answer_author_id, users.email as answer_author_email, users.name as answer_author_name, users.pro_pic_url as answer_author_pro_pic_urls')
       .joins('INNER JOIN answers ON questions.id = answers.question_id
       INNER JOIN users ON users.id = answers.author_id')
       .where("users.email LIKE ?", '%@insignia.vc').load
+    if params[:ds]
+      answer = answer.where('DATE(answers.created_at) >= ?', params[:ds])
+    end
+    if params[:de]
+      answer = answer.where('DATE(answers.created_at) <= ?', params[:de])
+    end
     return render :json => {
       total: answer.size,
       data: answer
